d76c7a385962788776327b7586bb303bd5af64d9
suffera <pavel.vutkow@gmail.com>
2013-11-06 22:30:58 +0200

    Create initi.php

---
+++ initi.php
@@ -0,0 +1,26 @@
+<?php
+
+
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host' => '127.0.0.1',
+        'username' => 'suffera',
+        'password' => '9873559p2',
+        'db' => 'myoop'
+    ),
+    'remember' => array(
+        'cookie_name' => 'hash', //име на бисквитката
+        'cookie_expiry' => 604800 //време на  действие
+    ),
+    'session' => array(
+        'session_name' => 'user'//името на сесията 
+    )
+);
+
+
+spl_autoload_register(function($class) {
+            require_once 'classes/' . $class . '.php';
+        });
+require_once 'functions/sanitize.php';
